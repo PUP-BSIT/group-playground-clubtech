cd90a00ce2613fc48f1292c8db8a670929cfd973
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2024-10-21 19:47:21 +0800

    add TODO lists

---
+++ main2.py
@@ -0,0 +1,4 @@
+ # TODO(John Matthew Arroyo): create a function that will let the customer input the order details
+ # TODO(Franco Lazaro): create a function that will calculate for the grand total (including the discount)
+ # TODO(John Carlo Nolluda): create a function that will get customer details
+ # TODO(John Matthew Arroyo): create a function that will display the receipt
